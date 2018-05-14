14f06b717b99f3e0202d423d82b77262189d597b
Jola <jolantafilipiak7@gmail.com>
2018-05-15 01:48:32 +0200

    Login and registration form, navbar, footer

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var registerRouter = require('./routes/register');
+var loginRouter = require('./routes/login');
+var passwordHelpRouter = require('./routes/password-help');
 
 var app = express();
 
@@ -21,6 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/register', registerRouter);
+app.use('/login', loginRouter);
+app.use('/password-help', passwordHelpRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
