4cbb38d82141b3a0f53bbfd86d9bb50f7c62c3c3
Jola <jolantafilipiak7@gmail.com>
2018-05-20 12:47:19 +0200

    Books view improved (including category/series/authors views)

--- routes/books.js
+++ routes/books.js
@@ -2,26 +2,6 @@ var express = require('express');
 var router = express.Router();
 const db = require('../db')
 
-router.get('/', function (req, res, next) {
-
-  const queryStatement = `SELECT * FROM book_info; `
-
-  db.query(queryStatement, (error, result) => {
-
-    if (result === null || result === undefined || result.length == 0) {
-      // console.log(JSON.stringify(result, null, 2))
-      const message = "Nie znaleziono książek w bazie"
-      res.send(message)
-      // res.render('resource_not_found', { message: message })
-      // res.render('page_not_found')
-    } else {
-      console.log(JSON.stringify(result, null, 2))
-      res.render('books', { booksArr: result })
-    }
-  })
-});
-
-
 router.get('/category/:category_id', function (req, res, next) {
 
   const queryStatement = `SELECT * FROM book_info WHERE category_id = ${req.params.category_id}; `
@@ -29,13 +9,11 @@ router.get('/category/:category_id', function (req, res, next) {
   db.query(queryStatement, (error, result) => {
 
     if (result === null || result === undefined || result.length == 0) {
-      // console.log(JSON.stringify(result, null, 2))
-      const message = "Nie znaleziono książek z takiej kategorii w bazie"
-      res.send(message)
+      
+      res.send("Nie znaleziono książek z takiej kategorii w bazie")
       // res.render('resource_not_found', { message: message })
-      // res.render('page_not_found')
-    } else {
-      console.log(JSON.stringify(result, null, 2))
+    } 
+    else {
       res.render('books', { booksArr: result })
     }
   })
@@ -48,13 +26,44 @@ router.get('/authors/:author_id', function (req, res, next) {
   db.query(queryStatement, (error, result) => {
 
     if (result === null || result === undefined || result.length == 0) {
-      // console.log(JSON.stringify(result, null, 2))
-      const message = "Nie znaleziono książek takiego autora w bazie"
-      res.send(message)
-      // res.render('resource_not_found', { message: message })
-      // res.render('page_not_found')
+
+      res.send("Nie znaleziono książek takiego autora w bazie")
+      
     } else {
-      console.log(JSON.stringify(result, null, 2))
+      res.render('books', { booksArr: result })
+    }
+  })
+});
+
+router.get('/series/:series_id', function (req, res, next) {
+
+  const queryStatement = `SELECT * FROM book_info WHERE series_id = ${req.params.series_id}; `
+
+  db.query(queryStatement, (error, result) => {
+
+    if (result === null || result === undefined || result.length == 0) {
+
+      res.send("Nie znaleziono książek z takiej serii w bazie")
+
+    } else {
+      res.render('books', { booksArr: result })
+    }
+  })
+});
+
+
+router.get('/', function (req, res, next) {
+
+  const queryStatement = `SELECT * FROM book_info; `
+
+  db.query(queryStatement, (error, result) => {
+
+    if (result === null || result === undefined || result.length == 0) {
+
+      res.send("Nie znaleziono książek w bazie")
+
+    } else {
+      // console.log(JSON.stringify(result, null, 2))
       res.render('books', { booksArr: result })
     }
   })
