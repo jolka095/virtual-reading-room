f8fac7cc5aded439edb8c965b29c24465e7a4c98
psychicsue <zuzia.kazior@gmail.com>
2018-05-21 02:14:32 +0200

    login and register pages

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db');
 
 router.get('/', function(req, res, next) {
   res.render('login', {  });
+});
+
+router.post('/', (req, res) => {
+
+    const sql=`SELECT username FROM users WHERE email = ${req.body.email} and password = ${req.body.password};`;
+
+    db.query(sql, function(err, results){
+        res.redirect('/user-profile');
+    });
 });
 
 module.exports = router;
--- routes/register.js
+++ routes/register.js
@@ -1,8 +1,36 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db');
 
 router.get('/', function(req, res, next) {
   res.render('register', {  });
+});
+router.get('/login', function(req, res, next) {
+  res.render('login', {  });
+});
+
+router.post('/', (req, res) => {
+
+    var user = {
+      "email":req.body.email,
+      "username":req.body.username,
+      "password":req.body.password
+    };
+
+    db.query(`INSERT into users SET ?`, user , function (err, rows, fields) {
+        if (err) {
+            console.log("error ocurred",err);
+            res.send({
+                "code":400,
+                "failed":"error ocurred"
+            })
+        }else{
+            res.send({
+                "code":200,
+                "success":"user registered sucessfully"
+            });
+        }
+    })
 });
 
 module.exports = router;
