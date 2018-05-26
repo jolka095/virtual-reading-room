ff4a71e37bc2f69b25384c1340237612e8ba2705
psychicsue <zuzia.kazior@gmail.com>
2018-05-26 14:47:35 +0200

    missing stylesheet and changes in login

--- routes/login.js
+++ routes/login.js
@@ -1,80 +1,79 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-var flash = require('connect-flash');
+const LocalStrategy = require('passport-local').Strategy;
 var session = require('express-session');
+const jwt = require('jsonwebtoken');
 
-LocalStrategy = require('passport-local').Strategy;
 const db = require('../db');
 
-router.get('/', function(req, res, next) {
-  res.render('login', {  });
-});
-
 router.use(session({
-    secret: '{secret}',
+    secret: 'secret',
     name: 'session_id',
-    saveUninitialized: true,
-    resave: true})
-);
+    saveUninitialized: false,
+    resave: true,
+    cookie  : { maxAge  : new Date(Date.now() + (60 * 1000 * 30 *3)) }
+}));
 
 router.use(passport.initialize());
 router.use(passport.session());
-router.use(flash());
+
+router.get('/', function(req, res, next) {
+  res.render('login-error', {  });
+});
 
 router.post('/', passport.authenticate('local', {
     successRedirect: '/library',
-    failureRedirect: '/login',
-    failureFlash: true,
-    successFlash: "Welcome!"}),
+    failureRedirect: '/login'})
 );
+
+passport.serializeUser(function(user, done){
+    done(null, user.idusers);
+});
+
+passport.deserializeUser(function(user, done){
+    db.query("SELECT * FROM users WHERE idusers=" + user.idusers, function (err, result){
+        done(err, result);
+    });
+});
 
 passport.use('local', new LocalStrategy({
         usernameField: 'email',
         passwordField: 'password',
-        passReqToCallback: true },
+        passReqToCallback: true
+    },
 
-    function (req, username, password, done){
-        if(!username || !password ) { return done(null, false, req.flash('message','All fields are required.')); }
+    function (req, username, password, done) {
 
-        const sql=`SELECT * FROM users WHERE email="${req.body.email}" and password="${req.body.password}";`;
+        if (!username || !password) {
+            return done(null, false);
+        }
+        console.log(username);
+        console.log(password);
 
-        db.query(sql, function(err, result){
 
-            if (err) return done(req.flash('message',err + req.flash('message','Invalid username or password.')));
+        const sql = `SELECT * FROM users WHERE email="${req.body.email}" and password="${req.body.password}";`;
 
-            if(!result.length){ return done(null, false, req.flash('message','Invalid username or password.')); }
+        db.query(sql, function (err, result) {
+
+            if (err) return done(err);
+
+            if (!result.length) {
+                return done(null, false);
+            }
 
             var encPassword = req.body.password;
-            var dbPassword  = result[0].password;
+            var dbPassword = result[0].password;
 
-            if(!(dbPassword === encPassword)){
-                return done(null, false, req.flash('message','Invalid username or password.'));
+            if (!(dbPassword === encPassword)) {
+                return done(null, false);
             }
+
+            console.log(result[0]);
 
             return done(null, result[0]);
         });
     }
 ));
-
-passport.serializeUser(function(user, done){
-    done(null, user.username);
-});
-
-passport.deserializeUser(function(user, done){
-
-    db.query("SELECT * FROM users WHERE username=" + user.username , function (err, rows){
-        done(err, rows);
-    });
-});
-
-function authenticationMiddleware(req, res, next) {
-    // if user is authenticated in the session, carry on
-    if (req.isAuthenticated())
-        return next();
-
-    // if they aren't redirect them to the home page
-    res.redirect('/');
-}
 
 module.exports = router;
