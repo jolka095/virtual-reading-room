81e36d4731c83cc54255ae2e3a57afed6e5c5d7c
Asia Ziobrowska <joanna.ziobrowska@student.uj.edu.pl>
2018-05-27 18:19:28 +0200

    marks add and change done

--- routes/book_profile.js
+++ routes/book_profile.js
@@ -11,6 +11,43 @@ router.get('/', (req, res, next) => {
 router.get('/:book_id', (req, res, next) => {
 
     const queryStatement = `SELECT * FROM book_info WHERE book_id = ${req.params.book_id}; `;
+    const queryStatement2 = `SELECT * FROM book_marks WHERE idbooks = ${req.params.book_id} AND idusers = ${req.user[0].idusers}; `;
+
+    db.query(queryStatement, (error, result) => {
+
+        if (result === null || result === undefined || result.length === 0) {
+            //   console.log(JSON.stringify(result[0], null, 2));
+            const message = "Nie znaleziono takiej ksiażki w bazie";
+            // res.render('resource_not_found', { message: message })
+            res.render('page_not_found', { user: req.user })
+        } else {
+            console.log(JSON.stringify(result[0], null, 2));
+            
+            db.query(queryStatement2, (error, result2) => {
+
+                if (result2 === null || result2 === undefined || result2.length === 0) {
+                     // console.log(JSON.stringify(result2[0], null, 2));
+                    const message = "Nie znaleziono oceny  w bazie";
+                    // res.render('resource_not_found', { message: message })
+                    res.render('book_profile', { book: result[0], user: req.user[0], mark: 10})
+
+                } else{
+                    if (req.user) {
+                        console.log("\nOK  ZALOGOWANY\n");
+                        res.render('book_profile', { book: result[0], user: req.user[0], mark: result2[0].idmarks })
+                    } else {
+                        console.log("\nX  NIEZALOGOWANY\n");
+                        res.render('book_profile', { book: result[0], user: null })
+                    }
+                }
+            })
+        }
+    })
+});
+
+router.get('/:book_id/:mark', (req, res, next) => {
+
+    const queryStatement = `SELECT * FROM book_info WHERE book_id = ${req.params.book_id}; `;
 
     db.query(queryStatement, (error, result) => {
 
@@ -24,7 +61,7 @@ router.get('/:book_id', (req, res, next) => {
 
             if (req.user) {
                 console.log("\nOK  ZALOGOWANY\n");
-                res.render('book_profile', { book: result[0], user: req.user[0] })
+                res.render('book_profile', { book: result[0], user: req.user[0], mark: req.params.mark})
             } else {
                 console.log("\nX  NIEZALOGOWANY\n");
                 res.render('book_profile', { book: result[0], user: null })
@@ -38,24 +75,36 @@ router.get('/:book_id', (req, res, next) => {
 router.post('/rate_book/:book_id/user/:user_id', (req, res, next) => {
 
     const mark = parseInt(req.body.mark) + 1;
-    const queryStatement = ` INSERT INTO book_marks (idbooks, idmarks, idusers)
+    const queryStatement1 = `DELETE FROM book_marks WHERE idusers = ${req.params.user_id} AND idbooks = ${req.params.book_id}`;
+    
+    const queryStatement = `INSERT INTO book_marks (idbooks, idmarks, idusers)
     VALUES ( ${req.params.book_id}, ${mark}, ${req.params.user_id} )`;
 
     console.log("queryStatement\n", queryStatement);
-
-    db.query(queryStatement, (error, result) => {
-
-        if (error) {
-            console.log("error ocurred", error);
+    db.query(queryStatement1, (error2, result2) => {
+        if (error2) {
+            console.log("error ocurred", error2);
             res.send(JSON.stringify({
                 "code": 400,
                 "failed": "error ocurred"
             }, null, 2))
         } else {
-            console.log(`Oceniono książkę ${req.params.book_id} przez usera ${req.params.user_id}`)
-            res.redirect(`/catalog/${req.params.book_id}`)
-        }
-    })
+            console.log(`Usunięto ${req.params.book_id} przez usera ${req.params.user_id}`)
+        db.query(queryStatement, (error, result) => {
+
+            if (error) {
+                console.log("error ocurred", error);
+                res.send(JSON.stringify({
+                    "code": 400,
+                    "failed": "error ocurred"
+                }, null, 2))
+            } else {
+                console.log(`Oceniono książkę ${req.params.book_id} przez usera ${req.params.user_id} na ${mark}`)
+                res.redirect(`/book_profile/${req.params.book_id}/${mark}`)
+            }
+        })
+    }
+})
 });
 
 router.post('/add_to_read/:book_id', function (req, res, next) {
